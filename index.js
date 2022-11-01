a52ff32ac5b3d8755ee45baef965aeab187c71d7
Elizaveta Kuskova <lizabet-ka@yandex.ru>
2022-11-02 00:00:41 +0300

    add

---
+++ index.js
@@ -0,0 +1,53 @@
+
+    let formElement = document.querySelector('.popup__form');
+    let nameInput = document.querySelector('.popup__name');
+    let jobInput = document.querySelector('.popup__job');
+    let nameProfil = document.querySelector('.profile__title');
+    let jobProfil = document.querySelector('.profile__info');
+    
+
+    let btnChangeName = document.querySelector('.profile__change-name');
+    let shadow = document.querySelector('.popup__shadow');
+    let btnClose = document.querySelector('.popup__btn-close');
+    let popup= document.querySelector('.popup');
+    let btnSave= document.querySelector('.popup__btn-save'); 
+
+function openPopup() {
+    popup.classList.add('popup_opened');
+    shadow.classList.add('popup__shadow-active');
+    nameInput.value = nameProfil.textContent;
+    jobInput.value = jobProfil.textContent;
+}; 
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+    shadow.classList.remove('popup__shadow-active');
+};
+
+function foolproof(){
+    if 
+    (!nameInput.value) alert('Заполните поле "Имя пользователя"');
+    else if 
+    (!jobInput.value) alert('Заполните поле "Вид деятельности"');
+    else 
+    nameProfil.textContent = nameInput.value;
+    jobProfil.textContent = jobInput.value;
+    closePopup(); 
+}
+// Обработчик «отправки» формы, она никуда отправляться не будет пока
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // отмен. станд. отправку формы.   
+
+    foolproof(); 
+    closePopup(); 
+};
+
+
+console.log();
+btnChangeName.addEventListener('click', openPopup);
+btnClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);  
+btnSave.addEventListener('click', foolproof);
+
+ 
+  
