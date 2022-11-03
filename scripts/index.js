1947e06ae65c88d324a5e22e4a1507c4eb0d0d90
Elizaveta Kuskova <lizabet-ka@yandex.ru>
2022-11-03 20:09:29 +0300

    fix:rev1-2

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,40 @@
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-job');
+let nameProfil = document.querySelector('.profile__title');
+let jobProfil = document.querySelector('.profile__info');
 
-    let formElement = document.querySelector('.popup__form');
-    let nameInput = document.querySelector('.popup__name');
-    let jobInput = document.querySelector('.popup__job');
-    let nameProfil = document.querySelector('.profile__title');
-    let jobProfil = document.querySelector('.profile__info');
-    
 
-    let btnChangeName = document.querySelector('.profile__change-name');
-    let btnClose = document.querySelector('.popup__btn-close');
-    let popup= document.querySelector('.popup');
-    let btnSave= document.querySelector('.popup__btn-save'); 
+let btnChangeName = document.querySelector('.profile__change-name');
+let btnClose = document.querySelector('.popup__btn-close');
+let popup = document.querySelector('.popup');
+let btnSave = document.querySelector('.popup__btn-save');
 
-function openPopup() {
+//ОТКРЫТИЕ
+function openedPopup() {
     popup.classList.add('popup_opened');
+    //текущее значение попап в окне импута присваиваем значение имени со страницы
     nameInput.value = nameProfil.textContent;
     jobInput.value = jobProfil.textContent;
 }; 
-
-function closePopup() {
-    popup.classList.remove('popup_opened');
-};
-
-/*function foolproof(){
-    if 
-    (!nameInput.value) alert('Заполните поле "Имя пользователя"');
-    else if 
-    (!jobInput.value) alert('Заполните поле "Вид деятельности"');
-    else 
+//ОБРАБОТКА ФОРМЫ
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // отмен. станд. отправку формы.
+    //в форме значение со страницы заменяется на введеное вручную значение
     nameProfil.textContent = nameInput.value;
     jobProfil.textContent = jobInput.value;
-
-}*/
-
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // отмен. станд. отправку формы.   
-
-    //foolproof(); 
-    closePopup(); 
 };
+//ЗАКРЫТИЕ
+function closedPopup() {
+    popup.classList.remove('popup_opened');
+};
+formElement
+//ЗАПУСК ФУНКЦИЙ
 
-
-console.log();
-btnChangeName.addEventListener('click', openPopup);
-btnClose.addEventListener('click', closePopup);
+btnChangeName.addEventListener('click', openedPopup);
+btnClose.addEventListener('click', closedPopup);
 formElement.addEventListener('submit', formSubmitHandler);  
-btnSave.addEventListener('click', foolproof);
+btnSave.addEventListener('click', closedPopup);
 
  
   
