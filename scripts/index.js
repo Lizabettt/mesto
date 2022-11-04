57f3fe2659139cea9fee661ba5544d45e438d84b
Elizaveta Kuskova <lizabet-ka@yandex.ru>
2022-11-04 10:13:39 +0300

    fix:rev1-3

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,11 @@ let nameInput = document.querySelector('.popup__input-name');
 let jobInput = document.querySelector('.popup__input-job');
 let nameProfil = document.querySelector('.profile__title');
 let jobProfil = document.querySelector('.profile__info');
-
-
 let btnChangeName = document.querySelector('.profile__change-name');
 let btnClose = document.querySelector('.popup__btn-close');
 let popup = document.querySelector('.popup');
 let btnSave = document.querySelector('.popup__btn-save');
+
 
 //ОТКРЫТИЕ
 function openedPopup() {
@@ -17,6 +16,7 @@ function openedPopup() {
     nameInput.value = nameProfil.textContent;
     jobInput.value = jobProfil.textContent;
 }; 
+
 //ОБРАБОТКА ФОРМЫ
 function formSubmitHandler (evt) {
     evt.preventDefault(); // отмен. станд. отправку формы.
@@ -28,13 +28,21 @@ function formSubmitHandler (evt) {
 function closedPopup() {
     popup.classList.remove('popup_opened');
 };
-formElement
-//ЗАПУСК ФУНКЦИЙ
 
+//ЗАПУСК ФУНКЦИЙ
 btnChangeName.addEventListener('click', openedPopup);
 btnClose.addEventListener('click', closedPopup);
 formElement.addEventListener('submit', formSubmitHandler);  
 btnSave.addEventListener('click', closedPopup);
 
  
-  
+  /* НЕ РАБОТАЕТ ПОКА
+function closedPopup(event) {
+    if (
+    event.target.classList.contains('.popup_opened') 
+    || event.target.classList.contains('.popup__btn-close')
+    ){
+    popup.classList.remove('popup_opened');
+    }
+};
+popup.addEventListener('click', closedPopup);*/
