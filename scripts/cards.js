6b015312d31e2c40dc981d561021a4291f0eac31
Elizaveta Kuskova <lizabet-ka@yandex.ru>
2022-12-08 12:55:30 +0300

    fix: pr6 rev1-2

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+        name: 'Парящий орёл',
+        link: 'https://images.unsplash.com/photo-1666112308178-d7511ed45436?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1758&q=80'
+    },
+    {
+        name: 'Слоны',
+        link: 'https://images.unsplash.com/photo-1666167469639-9d3f88757ef3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1273&q=80'
+    },
+    {
+        name: 'Лисица',
+        link: 'https://images.unsplash.com/photo-1666512244128-a69c6d115dce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        name: 'Жираф',
+        link: 'https://images.unsplash.com/photo-1666274802032-e1f9fb2356a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+    },
+    {
+        name: 'Обезьянка',
+        link: 'https://images.unsplash.com/photo-1666105956517-bb1aefae7842?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+    },
+    {
+        name: 'Совушка',
+        link: 'https://images.unsplash.com/photo-1665436664161-29a4468aed6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
+    }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,14 @@
-
-const popupPerson = document.querySelector('.popup_type-person');
+const popupProfile = document.querySelector('.popup_type-profile');
 const popupNewCard = document.querySelector('.popup_type-add-new-card');
 const popupImgMax = document.querySelector('.popup_type-img');
 
-const formChangePerson = document.querySelector('.popup__form_type-person');
+const formChangeProfile = document.querySelector('.popup__form_type-profile');
 const formAddNewCard = document.querySelector('.popup__form_type-add-new-card');
 
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
-const nameProfil = document.querySelector('.profile__title');
-const jobProfil = document.querySelector('.profile__info');
+const nameProfile = document.querySelector('.profile__title');
+const jobProfile = document.querySelector('.profile__info');
 
 const namePlaceInput = document.querySelector('.popup__input_type_name-place');
 const linkPlaceInput = document.querySelector('.popup__input_type_link-place');
@@ -17,39 +16,15 @@ const linkPlaceInput = document.querySelector('.popup__input_type_link-place');
 const closeBtns = document.querySelectorAll('.popup__btn-close');
 const btnChangeName = document.querySelector('.profile__change-name');
 const btnAddCard = document.querySelector('.profile__button-add');
+const btnCreate = document.querySelector('.popup__btn-create')
+const buttonLike = document.querySelector('.elements__like');
 
 const imgMaxCard = document.querySelector('.popup__img-max');
 const titleMaxCard = document.querySelector('.popup__img-title');
-const imgCard = document.querySelector('.elements__pic');
 
 const cardBox = document.querySelector('.elements__grid');
 const templateCard = document.querySelector('#elements-template').content.querySelector('.elements__card');
-const initialCards = [
-    {
-        name: 'Парящий орёл',
-        link: 'https://images.unsplash.com/photo-1666112308178-d7511ed45436?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1758&q=80'
-    },
-    {
-        name: 'Слоны',
-        link: 'https://images.unsplash.com/photo-1666167469639-9d3f88757ef3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1273&q=80'
-    },
-    {
-        name: 'Лисица',
-        link: 'https://images.unsplash.com/photo-1666512244128-a69c6d115dce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
-    },
-    {
-        name: 'Жираф',
-        link: 'https://images.unsplash.com/photo-1666274802032-e1f9fb2356a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-    },
-    {
-        name: 'Обезьянка',
-        link: 'https://images.unsplash.com/photo-1666105956517-bb1aefae7842?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
-    },
-    {
-        name: 'Совушка',
-        link: 'https://images.unsplash.com/photo-1665436664161-29a4468aed6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
-    }
-];
+
 
 //ОТКРЫТИЕ ПОПАПА
 function openPopup(namePopup) {
@@ -58,7 +33,9 @@ function openPopup(namePopup) {
 
 //ЗАКРЫТИЕ ПОПАПА
 function closePopup(namePopup) {
-    namePopup.classList.remove('popup_opened');    
+    namePopup.classList.remove('popup_opened');
+    document.removeEventListener("keydown", handleCloseByEscapePush); 
+    
 };
 
 //закрытие по кнопке
@@ -69,45 +46,52 @@ closeBtns.forEach((button) => {
 });
 
 //закрытие по эскейпу
-function closeEsc(evt) {
-    if (evt.keyCode === 27) {
-        const popupOpen = document.querySelector('.popup_opened')
-        closePopup(popupOpen);    
-    } 
-};
+function handleCloseByEscapePush(evt) {
+    if (evt.key === 'Escape') {
+        const popupOpen = document.querySelector('.popup_opened') 
+        closePopup(popupOpen);           
+        } 
+    };
 
 //закрытие по оверлею
-function closedPopupClickShadow(evt) {
-    if (evt.target.classList.contains('popup_opened')) {
-        const popupOpen = document.querySelector('.popup_opened')
-        closePopup(popupOpen); 
-    }
-    
-};
+function handleCloseByOverlayClick(evt) {
+    if (evt.target.classList.contains('popup_opened')) {       
+        closePopup(evt.target); 
+        }      
+    };
 
 
 //ОТКРЫТИЕ ПОПАПА ПРОФИЛЯ
 function openProfilePopup() {
-    nameInput.value = nameProfil.textContent;
-    jobInput.value = jobProfil.textContent;
-    openPopup(popupPerson);
+    nameInput.value = nameProfile.textContent;
+    jobInput.value = jobProfile.textContent;
+    openPopup(popupProfile);
+    
 };
 
 //ОТКРЫТИЕ ПОПАПА ДОБАВЛЕНИЯ НОВОЙ КАРТОЧКИ
-function addNewCard() {
+function openAddCardPopup() {
      openPopup(popupNewCard);
-};
+     btnCreate.disabled = true;
+    };
 
 //КАРТОЧКИ
 //удаление карточки
-const deleteCard = (evt) => {
+const handleDeleteClick = (evt) => {
     evt.target.closest('.elements__card').remove();
 }
 
 //лайк карточке
-const likeCard = (evt) => {
+
+const handleLikeClick = (evt) => {
     evt.target.classList.toggle('elements__like-add');
 }
+
+// function handleLikeClick(buttonLike) {
+//     buttonLike.classList.add('elements__like-add');
+// };
+  
+
 
 //создание новой карточки по образцу из "коробки"
 const createCard = (infoCard) => {
@@ -124,16 +108,16 @@ const createCard = (infoCard) => {
      
     function openImagePopup() {
           
-        imgMaxCard.src = imgCard.src;
-        imgMaxCard.alt = imgCard.alt ;
-        titleMaxCard.textContent = titleCard.textContent  ;
+        imgMaxCard.src = infoCard.link;
+        imgMaxCard.alt = infoCard.name ;
+        titleMaxCard.textContent = infoCard.name;
 
         openPopup(popupImgMax);
     };
 
-    btnDelCard.addEventListener('click', deleteCard);
-    btnAddLike.addEventListener('click', likeCard);
-    imgCard.addEventListener('click',openImagePopup);
+    btnDelCard.addEventListener('click', handleDeleteClick);
+    btnAddLike.addEventListener('click', handleLikeClick);
+    imgCard.addEventListener('click', openImagePopup);
 
     return newCard;
 }
@@ -150,18 +134,18 @@ initialCards.forEach(renderCard);
 
 // обработка формы пользователя
 
-function handleFormChangePerson (evt) {
+function handleProfileFormSubmit (evt) {
     evt.preventDefault(); // отмен. станд. отправку формы.
     
-    nameProfil.textContent = nameInput.value;//в форме значение со страницы заменяется на введеное вручную значение
-    jobProfil.textContent = jobInput.value;
+    nameProfile.textContent = nameInput.value;//в форме значение со страницы заменяется на введеное вручную значение
+    jobProfile.textContent = jobInput.value;
     
-    closePopup(popupPerson);   
+    closePopup(popupProfile);   
 };
 
 //обработка формы карточки
 
-function handleFormAddCard (evt) {
+function handleFormAddCardSubmit (evt) {
     evt.preventDefault();
 
     const infoCard = {
@@ -170,14 +154,16 @@ function handleFormAddCard (evt) {
     } 
     evt.target.reset();
     renderCard(infoCard);
-    closePopup(popupNewCard);    
+    closePopup(popupNewCard);
 };
 
 //ЗАПУСК ФУНКЦИЙ
 
 btnChangeName.addEventListener('click',openProfilePopup);
-btnAddCard.addEventListener('click', addNewCard);
-formChangePerson.addEventListener('submit', handleFormChangePerson);
-formAddNewCard.addEventListener('submit',handleFormAddCard);
-document.addEventListener('click', closedPopupClickShadow);//убирать обработчик?
-document.addEventListener("keydown", closeEsc); 
+btnAddCard.addEventListener('click', openAddCardPopup);
+formChangeProfile.addEventListener('submit', handleProfileFormSubmit);
+formAddNewCard.addEventListener('submit', handleFormAddCardSubmit);
+popupProfile.addEventListener('click', handleCloseByOverlayClick);
+popupNewCard.addEventListener('click', handleCloseByOverlayClick);
+popupImgMax.addEventListener('click', handleCloseByOverlayClick);
+document.addEventListener("keydown", handleCloseByEscapePush); 
